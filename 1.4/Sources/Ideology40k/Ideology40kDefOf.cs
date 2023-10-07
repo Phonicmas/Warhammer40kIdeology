88398089d44327e4b186dd2eb0fbbc0b3851cc5c
Phonicmas <jkskovlund@gmail.com>
2023-10-07 16:12:13 +0200

    Initial Release

--- 1.4/Sources/Ideology40k/Ideology40kDefOf.cs
+++ 1.4/Sources/Ideology40k/Ideology40kDefOf.cs
@@ -7,8 +7,10 @@ namespace Ideology40k
     [DefOf]
     public static class Ideology40kDefOf
     {
+        public static PreceptDef BEWH_Champion;
 
-        public static HediffDef BEWH_SecondGeneseedHarvest;
+        public static ThoughtDef BEWH_InteractedWithChampionMood;
+
         static Ideology40kDefOf()
         {
             DefOfHelper.EnsureInitializedInCtor(typeof(Ideology40kDefOf));
---
+++ 1.4/Sources/Ideology40k/ThoughtWorker_Champion.cs
@@ -0,0 +1,35 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Ideology40k
+{
+    public class ThoughtWorker_Champion : ThoughtWorker
+    {
+        protected override ThoughtState CurrentSocialStateInternal(Pawn pawn, Pawn other)
+        {
+            if (!other.RaceProps.Humanlike || !RelationsUtility.PawnsKnowEachOther(pawn, other))
+            {
+                return false;
+            }
+            if (pawn.ideo == null || other.ideo == null)
+            {
+                return false;
+            }
+            if (other.Ideo != pawn.Ideo)
+            {
+                return false;
+            }
+            if (other.Ideo.GetRole(other) == null)
+            {
+                return false;
+            }
+            if (other.Ideo.GetRole(other).def == Ideology40kDefOf.BEWH_Champion)
+            {
+                pawn.needs.mood.thoughts.memories.TryGainMemory(Ideology40kDefOf.BEWH_InteractedWithChampionMood);
+                return true;
+            }     
+
+            return false;
+        }
+    }
+}
